59e5d43b7f3f433517e07d7844ad231e8746e00a
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-02-16 22:14:04 +0530

    Initial Commit

---
+++ Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by Vikas Yadav on 16/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Vikas Yadav on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
